cf9ec678019391179da14df1997fbfa1c7282b93
pingkaihu <pingkai.hu@gmail.com>
2026-06-06 23:18:38 +0800

    refactor: remove obsolete error handling and week page redirection components

--- app/schedule_admin/[week]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-import { getCurrentMonthParam } from '@/lib/dates'
-
-export default async function WeekPage() {
-  redirect(`/schedule_admin/${getCurrentMonthParam()}`)
-}
